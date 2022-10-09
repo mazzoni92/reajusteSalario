e256f602ee799df02aa1ca869a541b9df153925f
Leandro <ldr_mt@hotmail.com>
2022-10-09 20:31:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double resultado = 0;
+double reajuste = 0;
+Console.WriteLine("Vamos verificar o reajuste do salario?");
+for(int pessoa = 0;pessoa < 10;pessoa++){
+    Console.WriteLine($"Digite o salario do {pessoa + 1}º ");
+    double salario = Convert.ToDouble(Console.ReadLine());
+    if(salario <= 300){
+        reajuste = (salario * 50) / 100;
+        resultado = reajuste + salario;
+    }
+    else{
+        reajuste = (salario * 30)/ 100;
+        resultado = reajuste + salario;
+    }
+    Console.WriteLine($"O salario reajustado do {pessoa + 1}º é R${reajuste} que fica R${resultado}");
+}
